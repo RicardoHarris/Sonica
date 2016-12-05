f3b195eda56975f3f9927ed485b5a2c75f30ee3e
Ricardo Harris <ricardoharris@ymail.com>
2016-12-05 13:27:50 -0800

    5th commit, 1st milestone without toasts (untested)
    Well. Got something done. Back on track

--- app/src/main/java/com/example/student/sonica/MainActivity.java
+++ app/src/main/java/com/example/student/sonica/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.student.sonica;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer song1;
+    Button playButtonVar;
+    Button pauseButtonVar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        song1 = MediaPlayer.create(this, R.raw.glaringlyablaze);
+        playButtonVar = (Button) findViewById(R.id.ply);
+        pauseButtonVar = (Button) findViewById(R.id.pse);
+    }
+    public void playSong(View view) {
+        song1.start();
+        pauseButtonVar.setEnabled(false);
+        playButtonVar.setEnabled(true);
+    }
+    public void pauseSong(View view) {
+        song1.start();
+        pauseButtonVar.setEnabled(true);
+        playButtonVar.setEnabled(false);
     }
 }
